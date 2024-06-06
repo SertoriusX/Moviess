546021359b3325a640149084e6a9c900e8a7d161
SertoriusX <tsvetan.georgiev891@gmail.com>
2024-06-06 20:15:53 +0200

    first commit

--- movie_online/settings.py
+++ movie_online/settings.py
@@ -122,10 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 if not DEBUG:
     STATIC_URL = 'static/'
-    STATICFILES_DIRS = [BASE_DIR /'static']
     MEDIA_URL ='/images/'
     MEDIA_ROOT=BASE_DIR / 'static/images'
     STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [BASE_DIR /'static']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
