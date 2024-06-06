3bc68041c0a8d1a326bd2ce76e7f7e4b5ea31f34
SertoriusX <tsvetan.georgiev891@gmail.com>
2024-06-06 19:58:19 +0200

    first commit

--- movie_online/urls.py
+++ movie_online/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('admin_profile/',include('is_admin.urls')),
     path('user_profile/',include('is_user.urls'))
 ]
-
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
